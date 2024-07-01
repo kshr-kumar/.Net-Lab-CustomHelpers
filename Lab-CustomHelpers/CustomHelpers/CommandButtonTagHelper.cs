cc25fdcc672e9893afaf6c0e396c8ee6a42fffe5
kishore <kishore@WIN-10>
2024-07-01 12:04:46 +0000

    Add project files.

---
+++ Lab-CustomHelpers/CustomHelpers/CommandButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Lab_CustomHelpers.CustomHelpers
+{
+    public class CommandButtonTagHelper: TagHelper
+    {
+        [HtmlAttributeName("value")]
+        public string Value { get; set; }
+        [HtmlAttributeName("class")]
+        public string CssClass { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string urBan = $"<button type='submit' name='command' class='{CssClass}'> {Value}</button>";
+            output.Content.SetHtmlContent(urBan);
+        }
+
+    }
+}
